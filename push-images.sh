b7407caaa47eb63e50eab284e3500a944e1b0fa6
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2024-11-23 12:35:07 -0500

    Publish v0.139.1. [release] (#400)

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push hubci/hugo:0.139
-docker push hubci/hugo:0.139.0
+docker push hubci/hugo:0.139.1
